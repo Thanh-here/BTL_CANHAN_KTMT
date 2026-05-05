23e08fb876fc955bf1a13ca40c87cf6e2dcd89ca
Thanh-here <buithuythanh359@gmail.com>
2026-05-05 19:25:20 +0700

    Enhance Bai1_1.asm with comments for clarity
    Added comments to clarify code functionality.

--- Bai1_1.asm
+++ Bai1_1.asm
@@ -3,30 +3,33 @@
 .Data
     English db "Hello$"
     VietNam db "Xin chao$"
-    crlf db 13, 10, '$' 
+    crlf    db 13, 10, '$' ; Ký tự về đầu dòng (13) và xuống dòng (10)
+
 .Code
 Main Proc    
-   
+    ; --- Khoi tao doan du lieu ---
     mov ax, @data
     mov ds, ax
     
-    mov ah, 9
-    lea dx, English
-    int 21h        
+    ; --- In chuoi tieng Anh ---
+    mov ah, 9           ; Ham 9 cua ngat 21h dung de in chuoi
+    lea dx, English     ; Lay dia chi bien English vao DX
+    int 21h             
     
+    ; --- In dau xuong dong ---
     lea dx, crlf
     int 21h
     
+    ; --- In chuoi tieng Viet ---
     lea dx, VietNam
     int 21h
     
-    mov ah, 4ch
+    ; --- Ket thuc chuong trinh ---
+    mov ah, 4ch         ; Ham 4ch dung de thoat ve DOS
     int 21h
-   
 Main endp 
 
 END MAIN
-
 
 
 
